969b08f5677ae3e47840bd0e100387dd89c07887
Dev Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2022-07-16 15:56:13 +0530

    Update wsgi.py

--- askadev/wsgi.py
+++ askadev/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askadev.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(settings.STATIC_ROOT, prefix='more-files/')
