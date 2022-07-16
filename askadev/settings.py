503bc8e01fbdd083b7e67f8fecce537e6d178d0e
Dev Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2022-07-16 16:06:33 +0530

    Update settings.py

--- askadev/settings.py
+++ askadev/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -140,3 +141,5 @@ REST_FRAMEWORK = {
         'api.serializers.PrettyJSONRender',
     ]
 }
+
+django_heroku.settings(locals())
